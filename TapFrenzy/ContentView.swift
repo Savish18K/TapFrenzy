448bfdb4c44474ca86242a6973113721c8ae5ebc
Savish18K <codse223f-040@student.nibm.lk>
2026-06-10 11:50:58 +0530

    Initial Commit

---
+++ TapFrenzy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TapFrenzy
+//
+//  Created by Savishka76 on 2026-06-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
